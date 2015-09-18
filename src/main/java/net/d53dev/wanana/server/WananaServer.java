36b02d4d58bf8e615c712729c533131775a9ccc0
David Sere <dave@d53dev.net>
2015-09-18 17:26:10 +0200

    Initial commit

---
+++ src/main/java/net/d53dev/wanana/server/WananaServer.java
@@ -0,0 +1,26 @@
+package net.d53dev.wanana.server;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+/**
+ * Created by davidsere on 18/09/15.
+ */
+public class WananaServer {
+    public static void main(final String[] args) {
+        Undertow server = Undertow.builder()
+                .addListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    @Override
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+                        exchange.getResponseSender().send("{\"message:\": \"LOL\"}");
+                    }
+                }).build();
+        server.start();
+
+        System.out.println("Started server at http://127.1:8080/  Hit ^C to stop");
+    }
+}
